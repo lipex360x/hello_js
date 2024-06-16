1d62a031c0c2a6839d79c152fb2a0cff1ec26373
Felipe Bambirra <lipex360@gmail.com>
2024-06-16 09:42:22 +0100

    lancamentoDAO added

---
+++ 11/src/LancamentoDAO.js
@@ -0,0 +1,18 @@
+import cuid from 'cuid'
+
+export class LancamentoDAO {
+  
+  constructor (connection) {
+    this.connection = connection
+  }
+
+  async getLancamento () {
+    const lancamentos = await this.connection.query("select * from branas.lancamento", [])
+    return lancamentos
+  }
+
+  async saveLancamento (lancamento) {
+    const id = cuid()
+    await this.connection.query('insert into branas.lancamento (lancamento_id, mes, categoria, tipo, valor) values ($1, $2, $3, $4, $5)', [ id, lancamento.mes, lancamento.categoria, lancamento.tipo, lancamento.valor ])
+  }
+}
--- 11/src/server.js
+++ 11/src/server.js
@@ -1,23 +1,23 @@
 import express from 'express'
 import cors from 'cors'
-import cuid from 'cuid'
 import { Connection } from './Connection'
+import { LancamentoDAO } from './LancamentoDAO'
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 const connection = new Connection()
+const lancamentoDAO = new LancamentoDAO(connection)
 
 app.get('/api/lancamentos', async (request, response) => {
-  const lancamentos = await connection.query("select * from branas.lancamento", [])
+  const lancamentos = await lancamentoDAO.getLancamento()
   response.json(lancamentos)
 })
 
 app.post('/api/lancamentos', async (request, response) => {
   const lancamento = request.body
-  const id = cuid()
-  await connection.query('insert into branas.lancamento (lancamento_id, mes, categoria, tipo, valor) values ($1, $2, $3, $4, $5)', [ id, lancamento.mes, lancamento.categoria, lancamento.tipo, lancamento.valor ])
+  await lancamentoDAO.saveLancamento(lancamento)
   response.end()
 })
 
